4a167e4d8489cc056689b80b5e97185e25b038e7
MichaelHope <mhsetriakor720@gmail.com>
2017-05-14 01:30:25 +0000

    first commit

---
+++ sample.java
@@ -0,0 +1,18 @@
+/**
+ * Created by SETRIAKOR MICHAEL on 5/9/2017.
+ */
+import java.util.*;
+
+public class sample {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double fnum, snum, product;
+        System.out.println("Enter first num: ");
+        fnum = input.nextDouble();
+        System.out.println("Enter second num: ");
+        snum = input.nextDouble();
+        product = fnum * snum;
+        System.out.println(product);
+    }
+
+}
